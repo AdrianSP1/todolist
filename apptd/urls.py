f0c51b7397190610eec0e66eb5a7426f1243a466
xiod <adriangustavose@gmail.com>
2021-08-31 21:35:58 -0300

    CRUD-V1.0

--- apptd/urls.py
+++ apptd/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+   
     path("agregar/", views.agregar, name="agregar"),
+   
+    path("eliminar/<int:tarea_id>/", views.eliminar, name="eliminar"),
+   
+    path("editar/<int:tarea_id>/", views.editar, name="editar")
 ]
